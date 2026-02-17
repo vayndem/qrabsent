d665ee99bacc1697804ebf62c3fdeef3d4d9ddf5
vayndem <abrorhaikal@gmail.com>
2026-02-17 21:35:14 +0700

    Fix Database

--- config/database.php
+++ config/database.php
@@ -58,9 +58,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => [
+                \PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+                \PDO::MYSQL_ATTR_SSL_CA => base_path('storage/isrgrootx1.pem'),
+            ],
         ],
 
         'mariadb' => [
@@ -80,7 +81,7 @@ return [
             'engine' => null,
             'options' => [
                 \PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-                \PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') ? base_path(env('MYSQL_ATTR_SSL_CA')) : null,
+                \PDO::MYSQL_ATTR_SSL_CA => base_path('storage/isrgrootx1.pem'),
             ],
         ],
 
